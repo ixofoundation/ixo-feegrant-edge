d77c3753809521b6a1c43abff8ff59c699414b0e
Casey Pazera <ParkourPunk@googlemail.com>
2022-11-16 12:12:07 +0000

    fix: updated feegrant to use new sdk

--- src/feegrant.ts
+++ src/feegrant.ts
@@ -1,44 +1,84 @@
-import { Coin } from './../node_modules/@cosmjs/proto-signing/node_modules/cosmjs-types/cosmos/base/v1beta1/coin.d';
-import { Timestamp } from './../node_modules/@cosmjs/proto-signing/node_modules/cosmjs-types/google/protobuf/timestamp.d';
-import { ixo,cosmos } from "@ixo/impactxclient-sdk";
+import { Coin } from "./../node_modules/@cosmjs/proto-signing/node_modules/cosmjs-types/cosmos/base/v1beta1/coin.d";
+import { Timestamp } from "./../node_modules/@cosmjs/proto-signing/node_modules/cosmjs-types/google/protobuf/timestamp.d";
+import {
+  ixo,
+  cosmos,
+  createQueryClient,
+  createSigningClient,
+} from "@ixo/impactxclient-sdk";
 import { getOfflineSignerProto as getOfflineSigner } from "cosmjs-utils";
 import { StdFee } from "@cosmjs/amino";
-const { createRPCQueryClient } = ixo.ClientFactory;
-const client = await createRPCQueryClient({ rpcEndpoint: RPC_ENDPOINT });
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 
-// now you can query the cosmos modules
-const balance = await client.cosmos.bank.v1beta1.allBalances({
-  address: "ixo1addresshere",
-});
+const feeGrant = async (grantee: string) => {
+  const mnemonic =
+    "unfold client turtle either pilot stock floor glow toward bullet car science";
 
-import { chains } from "chain-registry";
+  const signer = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
+    prefix: "ixo",
+  });
 
-const mnemonic =
-  "unfold client turtle either pilot stock floor glow toward bullet car science";
-const chain = chains.find(({ chain_name }) => chain_name === "impacthub");
-const signer = await getOfflineSigner({
-  mnemonic,
-  chain,
-});
+  const client = await createSigningClient(
+    "https://testnet.ixo.earth/rpc/",
+    signer
+  );
 
+  const BasicAllowance = cosmos.feegrant.v1beta1.BasicAllowance;
+  const address = await signer.getAccounts();
+  let timestamp = Timestamp.fromPartial({ seconds: 1, nanos: 1 });
+  let spendlimit = Coin.fromPartial({ denom: "ixo", amount: "1" });
 
-const { fromPartial } = cosmos.feegrant.v1beta1.BasicAllowance;
+  const allowance = {
+    typeUrl: "/cosmos.feegrant.v1beta1.BasicAllowance",
+    value: BasicAllowance.fromJSON({
+      spendLimit: [spendlimit],
+      expiration: timestamp,
+    }),
+  };
 
-let timestamp = Timestamp.fromPartial({seconds:1,nanos:1});
-let spendlimit = Coin.fromPartial({denom:"ixo",amount:"1"});
+  const grant = cosmos.feegrant.v1beta1.Grant;
+  const message = {
+    typeUrl: "/cosmos.feegrant.v1beta1.Grant",
+    value: grant.fromJSON({
+      allowance: allowance,
+      grantee: grantee,
+      granter: address[0].address,
+    }),
+  };
 
-const msg = fromPartial({spendLimit:[spendlimit],expiration:timestamp})
+  const gasUsed = await client.simulate(
+    address[0].address,
+    [message],
+    "feegrant-daemon-ixo"
+  );
 
-const fee: StdFee = {
-  amount: [
-    {
-      denom: "ixo",
-      amount: "864",
-    },
-  ],
-  gas: "86364",
+  const gasPriceStep = {
+    low: 0.01,
+    average: 0.025,
+    high: 0.04,
+  };
+
+  const low = gasUsed * gasPriceStep.low;
+  const average = gasUsed * gasPriceStep.average;
+  const high = gasUsed * gasPriceStep.high;
+
+  const gas = {
+    low: low,
+    average: average,
+    high: high,
+  };
+
+  const fee = {
+    amount: [
+      {
+        denom: "uixo",
+        amount: gas.high.toString(),
+      },
+    ],
+    gas: gasUsed.toString(),
+  };
+
+  const response = client.signAndBroadcast(address[0].address, [message], fee);
+
+  return response;
 };
-
-const response = await client.signAndBroadcast(address, [msg], fee);
-
-
